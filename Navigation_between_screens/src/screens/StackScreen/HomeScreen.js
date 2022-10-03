560b0318ab1e65807f87103ee8f2fd626ba6a4e3
Justinas Zailskas <j.zailskas@eif.viko.lt>
2022-10-03 20:35:57 +0300

    Revert "Redux Core example"
    This reverts commit 0c2929774c80f7c2c88aa48dd66bccfad925e35a.

--- Navigation_between_screens/src/screens/StackScreen/HomeScreen.js
+++ Navigation_between_screens/src/screens/StackScreen/HomeScreen.js
@@ -8,14 +8,11 @@ import {
   View,
   Button
 } from 'react-native';
-import { useSelector } from 'react-redux';
 
 function HomeScreen({navigation: { navigate }}) {
-  const counter_value = useSelector((state) => state.value);
     return (
         <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
            <Text>It is home page</Text>
-           <Text>Values is: {counter_value.value}</Text>
            <Button
               title="Go to secondStackPage"
               onPress={() => navigate('SecondStackPage')}
